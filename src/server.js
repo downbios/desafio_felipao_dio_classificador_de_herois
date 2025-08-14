fea4ec5694d116d335668e0363cf5b7a049f0664
IAN ADSON PINTO DE SOUZA VIEIRA <ian.vieira@SISTEMAFIEPE.LOCAL>
2025-08-13 22:16:07 -0300

    classificador de heroi

---
+++ src/server.js
@@ -0,0 +1,30 @@
+let herois = [
+  { nome: "Aragorn", xp: 8500 },
+  { nome: "Legolas", xp: 4500 },
+  { nome: "Gimli", xp: 1200 },
+  { nome: "Gandalf", xp: 15000 },
+];
+
+for (let heroi of herois) {
+  let nivel = "";
+
+  if (heroi.xp < 1000) {
+    nivel = "Ferro";
+  } else if (heroi.xp >= 1001 && heroi.xp <= 2000) {
+    nivel = "Bronze";
+  } else if (heroi.xp >= 2001 && heroi.xp <= 5000) {
+    nivel = "Prata";
+  } else if (heroi.xp >= 5001 && heroi.xp <= 7000) {
+    nivel = "Ouro";
+  } else if (heroi.xp >= 7001 && heroi.xp <= 8000) {
+    nivel = "Platina";
+  } else if (heroi.xp >= 8001 && heroi.xp <= 9000) {
+    nivel = "Ascendente";
+  } else if (heroi.xp >= 9001 && heroi.xp <= 10000) {
+    nivel = "Imortal";
+  } else if (heroi.xp >= 10001) {
+    nivel = "Radiante";
+  }
+
+  console.log(`O Herói de nome ${heroi.nome} está no nível de ${nivel}`);
+}
